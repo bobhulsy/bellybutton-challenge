2fe114f2f1e49b8e738ed5ab0732c08de5170f8d
bobhulsy <57780185+bobhulsy@users.noreply.github.com>
2024-03-19 06:48:30 -0700

    Add function to update charts and fix typo in function name

--- static/js/app.js
+++ static/js/app.js
@@ -56,3 +56,69 @@ function buildMetadata(sample) {
     });
 }
 
+// Function to update the charts
+function updateCharts(sample) {
+    d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+        var samples = data.samples;
+        var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        // Build a Bubble Chart
+        var bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: { t: 0 },
+            hovermode: "closest",
+            xaxis: { title: "OTU ID" },
+            margin: { t: 30}
+        };
+        var bubbleData = [
+            {
+                x: otu_ids,
+                y: sample_values,
+                text: otu_labels,
+                mode: "markers",
+                marker: {
+                    size: sample_values,
+                    color: otu_ids,
+                    colorscale: "Earth"
+                }
+            }
+        ];
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+        // Build a Bar Chart
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var barData = [
+            {
+                y: yticks,
+                x: sample_values.slice(0, 10).reverse(),
+                text: otu_labels.slice(0, 10).reverse(),
+                type: "bar",
+                orientation: "h",
+            }
+        ];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: { t: 30, l: 150 }
+        };
+
+        Plotly.newPlot("bar", barData, barLayout);
+    });
+}
+
+// Function to handle change on dropdown menu selection
+function optionChanged(newSample) {
+    console.log(newSample);
+    // Update the charts with new data
+    buildpCharts(newSample);
+    // Update the demographic info with new data
+    buildMetadata(newSample);
+    buildGauge(newSample)
+
+}
