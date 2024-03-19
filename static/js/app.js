7d34ec70d8b7cac100341d5bb8ed9b5290e13f37
bobhulsy <57780185+bobhulsy@users.noreply.github.com>
2024-03-19 06:19:50 -0700

    Add D3 library and populate dropdown menu with sample names

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+import * as d3 from 'd3';
 // Establish url as a global variable
 const url = ("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json");
 
@@ -11,3 +12,23 @@ function init() {
 
     //Use D3 to select the dropdown menu
     let dropdownmenu = d3.select("#selDataset");
+    //D3 to pull sample names from the data for the dropdown menu
+    d3.json(url).then((data) => {
+        
+        //Set sample names as variable
+        let names = data.names;
+
+        //Append the sample names to the dropdown menu
+        names.forEach((id)) => {
+
+            //Append the value of id for each sample to the dropdown menu
+            console.log(id);
+
+            dropdownmenu.append("option").text(id).property("value", id);
+        }
+    });
+
+    
+        
+        )
+
